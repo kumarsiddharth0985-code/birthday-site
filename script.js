8aa104dc917053ae83467610375817f7b678b22a
kumarsiddharth0985-code <kumarsiddharth0985@gmail.com>
2026-05-03 20:00:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,46 +7,30 @@ setTimeout(() => goToPage(2), 2000);
 
 function turnOnLight() {
     goToPage(5);
+    document.getElementById("page5").classList.add("lightPink");
 }
 
-/* MUSIC */
 function playMusic() {
     document.getElementById("music").play();
     document.getElementById("btnMusic").style.display = "none";
     document.getElementById("btnDecor").classList.remove("hidden");
 }
 
-/* DECOR */
 function decorate() {
     document.getElementById("decor").innerHTML = "🎈 HAPPY BIRTHDAY 🎈";
     document.getElementById("btnDecor").style.display = "none";
     document.getElementById("btnCake").classList.remove("hidden");
 }
 
-/* SHOW CAKE */
 function showCake() {
     document.getElementById("cakeBox").classList.remove("hidden");
     document.getElementById("btnCake").style.display = "none";
 
-    cutCake();
-}
-
-/* CUT ANIMATION */
-function cutCake() {
-    let line = document.getElementById("cutLine");
+    setTimeout(() => {
+        document.getElementById("cutLine").style.height = "200px";
+    }, 500);
 
     setTimeout(() => {
-        line.style.height = "200px"; // cut animation
-
-        setTimeout(() => {
-            document.getElementById("letter").classList.remove("hidden");
-        }, 1200);
-
-    }, 500);
-}
-
-/* OPEN LETTER */
-function openLetter() {
-    document.getElementById("letter").classList.add("hidden");
-    document.getElementById("messagePage").classList.remove("hidden");
+        document.getElementById("finalPopup").classList.remove("hidden"); // 🔥 FIX
+    }, 1500);
 }
